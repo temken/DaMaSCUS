3f969c415692d426b4243536840636c59b88a7a4
Timon Emken <emken@adm-107512-mac.pc.sdu.dk>
2017-08-21 16:25:00 +0200

    Added .get_code_cov.sh bash script for codecov.

---
+++ .get_code_cov.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for filename in `find . -type f -name '*.cpp'`; 
+do 
+	echo $filename
+	obj=$(echo $filename | sed -e s/src/build/ -e s/cpp/o/)
+	gcov-5 -r -p -o "$obj" "$filname"
+done
